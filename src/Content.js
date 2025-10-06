9f13c3b5ca25566c5b4078de926db3aebdad4d57
marlar-tz <marlarhal55@gmail.com>
2025-10-07 04:03:56 +0800

    Added Click events for active and inactive buttons

--- src/Content.js
+++ src/Content.js
@@ -1,56 +1,57 @@
-import {useEffect,useState} from 'react';
+import { useEffect, useState } from 'react';
+import ExtensionLists from './ExtensionLists';
 
 const Content = () => {
 
-    const [data,setData] = useState([]);
-    const [error,setError] = useState(null);
+    const [allData, setAllData] = useState([]);
+    const [error, setError] = useState(null);
+    const [data, setData] = useState([]);
 
-    useEffect(()=>{
-        
-            fetch('/data.json')
+    useEffect(() => {
+
+        fetch('/data.json')
             .then(res => {
-                if(!res.ok){
+                if (!res.ok) {
                     throw Error('could not fetch the data from that resource');
                 }
-                return res.json()})
-            .then(fetchData => setData(fetchData))
-            .catch(err=>{
+                return res.json()
+            })
+            .then(fetchData => {
+                setAllData(fetchData);
+                setData(fetchData);
+                console.log(Object.keys(fetchData).length);
+            })
+            .catch(err => {
                 setError(err.message)
             })
-        },[]);
+    }, []);
+
+    const filterData = (status) => {
+        if (status === 'all') {
+            setData(allData);
+        } else {
+            const newData = allData.filter(d => d.isActive === status);
+            setData(newData);
+           
+        }
+    }
 
     return (
         <div className='w-3/4 max-md:w-5/6 h-screen' >
+
             <div className='flex max-sm:flex-col py-4 items-center justify-between'>
                 <h1 className='text-xl font-bold '>Extensions List</h1>
                 <div className="max-sm:my-4">
-                    <button className='border border-neutral-400 hover:bg-red-300 px-3 py-1 rounded-full max-sm:mr-2 '>All</button>
-                    <button className='border border-neutral-400 hover:bg-red-300 px-3 py-1 ml-2 rounded-full max-sm:mr-2'>Active</button>
-                    <button className='border border-neutral-400 hover:bg-red-300 px-3 py-1 ml-2 rounded-full'>Inactive</button>
+
+                    <button onClick={() => filterData("all")} className='border border-neutral-400 hover:bg-red-300 px-3 py-1 rounded-full max-sm:mr-2 '>All</button>
+                    <button onClick={() => filterData(true)} className='border border-neutral-400 hover:bg-red-300 px-3 py-1 ml-2 rounded-full max-sm:mr-2'>Active</button>
+                    <button onClick={() => filterData(false)} className='border border-neutral-400 hover:bg-red-300 px-3 py-1 ml-2 rounded-full'>Inactive</button>
+
+
                 </div>
             </div>
-            {error && <div>{error} </div>}
-            <div className='grid grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 gap-x-4'>
-                {data &&
-                    data.map(ext => (
-                        <div className="border-2 border-purple-400 flex flex-col px-3 h-42 mb-4" key={ext.name}>
-                            <div className='flex justify-center items-start h-36 pt-4 px-3'>
-                                <img className='object-scale-down' src={ext.logo} alt="logo" />
-                                <div className='ml-2'>
-                                    <p>{ext.name}</p>
-                                    <p>{ext.description}</p>
-
-                                </div>
-                            </div>
-                            <div className='flex justify-between items-center px-3 mb-4'>
-                                <button className='border border-green-400 py-1 px-2 rounded-full'>Remove</button>
-                                <input className="switch" type="checkbox" defaultChecked={ext.isActive} />
-                            </div>
-
-                        </div>
-                    ))
-                }
-            </div>
+            {error && <div>{error}</div>}
+            <ExtensionLists data={data} />
         </div>
     )
 }
---
+++ src/ExtensionLists.js
@@ -0,0 +1,32 @@
+
+const ExtensionLists = (props) => {
+
+    const data = props.data;
+
+    return(
+        <div className='grid grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 gap-x-4'>
+                {data &&
+                    data.map(ext => (
+
+                        <div className="border-2 border-purple-400 flex flex-col px-3 h-42 mb-4" key={ext.name}>
+                            <div className='flex justify-center items-start h-36 pt-4 px-3'>
+                                <img className='object-scale-down' src={ext.logo} alt="logo" />
+                                <div className='ml-2'>
+                                    <p>{ext.name}</p>
+                                    <p>{ext.description}</p>
+
+                                </div>
+                            </div>
+                            <div className='flex justify-between items-center px-3 mb-4'>
+                                <button className='border border-green-400 py-1 px-2 rounded-full'>Remove</button>
+                                <input className="switch" type="checkbox" defaultChecked={ext.isActive} />
+                            </div>
+
+                        </div>
+                    ))
+                }
+            </div>
+    )
+}
+
+export default ExtensionLists;
